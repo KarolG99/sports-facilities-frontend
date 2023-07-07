bb8445abcc29361badeebf198958bef7334511ea
KarolG99 <karolus2212@gmail.com>
2023-07-07 14:49:21 +0200

    Handle toggle views

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,14 +3,25 @@ import { styled } from "styled-components";
 import Heading from "../components/atoms/Heading";
 
 import dictionary from "../dictionaries/pages.json";
-import ToggleButton from "../components/atoms/ToggleButton";
+import ToggleView from "../components/atoms/ToggleView";
+import { useState } from "react";
+
+export enum ViewsType {
+  LIST_VIEW = "listView",
+  MAP_VIEW = "mapView",
+}
 
 const HomePage = () => {
+  const [selectedView, setSelectedView] = useState(ViewsType.LIST_VIEW);
   const homeDictionary = dictionary.home;
   return (
     <StyledMain>
       <Heading level={1} title={homeDictionary.title} />
-      <ToggleButton />
+      <ToggleView
+        selectedView={selectedView}
+        setListView={() => setSelectedView(ViewsType.LIST_VIEW)}
+        setMapView={() => setSelectedView(ViewsType.MAP_VIEW)}
+      />
     </StyledMain>
   );
 };
@@ -20,6 +31,7 @@ const StyledMain = styled.main`
     margin: 10px auto;
     padding: 0;
     width: fit-content;
+    text-align: center;
   }
 `;
 
